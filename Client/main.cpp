78d3b70445c42d1a379284fb959a861b44c4b692
6yakaxD <142536957+6yakaxD@users.noreply.github.com>
2024-02-26 16:07:11 +0300

    Add files via upload

---
+++ Client/main.cpp
@@ -0,0 +1,27 @@
+﻿﻿/**
+ * @date        23-12-2023
+ * @brief       Casuall template of Boost
+ *              c++ client which server is
+ *              the boost async tcp c++
+ *              server 
+ * @author      Ramil
+ * @copyright   (C) 2023 by not commercial
+ */
+
+#include <iostream>
+#include "Client.h"
+
+int
+main()
+{
+  if (g_Net.connectToServer("127.0.0.1", "228"))
+  {
+    g_Net.Identify("kassa_po");
+  }
+  else
+  {
+    std::cout << "Server connection error" << std::endl;
+  }
+
+  return (0);
+}
